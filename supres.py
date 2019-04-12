b99faed196ecf2be362e471457e8d81bbd754c2f
stoictrader <28746824+stoictrader@users.noreply.github.com>
2019-04-12 08:12:12 +0530

    Create supres.py

---
+++ supres.py
@@ -0,0 +1,59 @@
+def supres(low, high, min_touches=2, stat_likeness_percent=1.5, bounce_percent=5):
+    """Support and Resistance Testing
+
+    Identifies support and resistance levels of provided price action data.
+
+    Args:
+        low(pandas.Series): A pandas Series of lows from price action data.
+        high(pandas.Series): A pandas Series of highs from price action data.
+        min_touches(int): Minimum # of touches for established S&R.
+        stat_likeness_percent(int/float): Acceptable margin of error for level.
+        bounce_percent(int/float): Percent of price action for established bounce.
+    
+    ** Note **
+        If you want to calculate support and resistance without regard for
+        candle shadows, pass close values for both low and high.
+
+    Returns:
+        sup(float): Established level of support or None (if no level)
+        res(float): Established level of resistance or None (if no level)
+    """
+    # Setting default values for support and resistance to None
+    sup = None
+    res = None
+    
+    # Identifying local high and local low
+    maxima = high.max()
+    minima = low.min()
+    
+    # Calculating distance between max and min (total price movement)
+    move_range = maxima - minima
+    
+    # Calculating bounce distance and allowable margin of error for likeness
+    move_allowance = move_range * (stat_likeness_percent / 100)
+    bounce_distance = move_range * (bounce_percent / 100)
+    
+    # Test resistance by iterating through data to check for touches delimited by bounces
+    touchdown = 0
+    awaiting_bounce = False
+    for x in range(0, len(high)):
+        if abs(maxima - high[x]) < move_allowance and not awaiting_bounce:
+            touchdown = touchdown + 1
+            awaiting_bounce = True
+        elif abs(maxima - high[x]) > bounce_distance:
+            awaiting_bounce = False
+    if touchdown >= min_touches:
+        res = maxima
+        
+    # Test support by iterating through data to check for touches delimited by bounces
+    touchdown = 0
+    awaiting_bounce = False
+    for x in range(0, len(low)):
+        if abs(low[x] - minima) < move_allowance and not awaiting_bounce:
+            touchdown = touchdown + 1
+            awaiting_bounce = True
+        elif abs(low[x] - minima) > bounce_distance:
+            awaiting_bounce = False
+    if touchdown >= min_touches:
+        sup = minima
+    return sup,res
